f11ddbc5af523a9249be6d9bbd17d606161fc768
Christian Langrock <clangrock@googlemail.com>
2022-07-26 22:54:48 +0200

    update readme and config_smple.conf

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Description
-This project measures the wind in km/h and reports the value via MQTT. I used that for retrieving my blind if the wind is 4 BFT (20 km/h) or higher.
+This project measures the wind in km/h and reports the value via MQTT.
+I used that for retrieving my blind if the wind is 8 BFT (70 km/h) or higher.
 
 # Hardware
 The following hardware is used for building this project:
 * ESP8266 (Wemos D1)
-* Eltako Windsensor (http://amzn.to/2ps1Tte - German Amazon)
+* PCE Instruments PCE-WS P Anemometer https://www.conrad.de/de/p/pce-instruments-pce-ws-p-anemometer-2452812.html
 
 # Wiring
 I connected the anemometer with the D7 and GND Pin of my Wemos D1. But it can be all other suitable Pins used.
---
+++ myconfig_sample.h
@@ -0,0 +1,42 @@
+/*************************************************/
+/* Debugging                                     */
+/*************************************************/
+constexpr bool debugOutput {true};  // set to true for serial OUTPUT
+
+/*************************************************/
+/* Settings for WLAN                             */
+/*************************************************/
+constexpr char* ssid {"xxxxxx"};
+constexpr char* password {"xxxx"};
+
+/*************************************************/
+/* Pin config                                    */
+/*************************************************/
+constexpr int input_pin_wind  {2};
+constexpr int output_pin_Relay_1 {14};
+constexpr int output_pin_Relay_2 { 12 };
+constexpr int DS18B20_PIN { 4 };     // Pin for DS18B20 on Arduino D2   
+constexpr int TEMPERATURE_PRECISION { 9 };
+
+/*************************************************/
+/* max wind speed                                */
+/*************************************************/
+constexpr int Config_max_wind_speed  {60};
+constexpr int Config_max_max_wind_speed {70};
+constexpr int Config_wind_hyst {10};
+constexpr int Config_timeMaxMaxWindSpeed {300}; // time to open the blinds
+constexpr int Config_timeMaxWindSpeed {60};     // time delay for the max wind speed limit, if the time is elapsed open the blinds
+/*************************************************/
+/* OTA Update settings (Over the air update)     */
+/*************************************************/ 
+constexpr char* firmware_version = "myanemometer_0.0.1";
+constexpr char* update_server = "myhost";
+constexpr char* update_uri = "/path/update.php";
+
+/*************************************************/
+/* MQTTCloud data                               */
+/*************************************************/
+constexpr char* mqtt_host { "xxx.xxx.xxx.xxx" };
+constexpr char* mqtt_id { "ESP8266-Anemometer" };
+constexpr char* mqtt_topic_prefix { "/weatherstation/wind" };
+constexpr int mqtt_port { 1883 };
